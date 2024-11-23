63e998bd2bf7bc5ebf80abd2ebd329aed0a52c0f
manishbsta <bistamanish681@gmail.com>
2024-11-23 22:14:17 +0545

    refactor: modify root layout for hydrating redux store

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,11 @@
 import '@src/unistyles';
 
+import { StorageKeys } from '@src/core/constants/storage-keys';
 import RootProvider from '@src/core/providers/RootProvider';
+import { useAppDispatch } from '@src/store/hooks';
+import { setPlans } from '@src/store/slices/app.slice';
+import { Plan } from '@src/types/plan';
+import { getItemFromStorage } from '@src/utils/expo-secure-store';
 import { Slot } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
@@ -21,9 +26,27 @@ const AppLayout = () => {
   return (
     <RootProvider>
       <StatusBar style='auto' />
-      <Slot />
+      <AppNavigation />
     </RootProvider>
   );
+};
+
+const AppNavigation = () => {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    const hydration = async () => {
+      const plansResult = await getItemFromStorage(StorageKeys.PLANS);
+      if (plansResult) {
+        const plans = JSON.parse(plansResult) as Plan[];
+        dispatch(setPlans(plans));
+      }
+    };
+
+    hydration();
+  }, [dispatch]);
+
+  return <Slot />;
 };
 
 export default AppLayout;
