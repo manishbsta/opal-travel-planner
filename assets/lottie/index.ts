81116f45e0b79480a90f608c4d0f63d40b843fba
manishbsta <bistamanish681@gmail.com>
2024-11-25 14:33:41 +0545

    chore: add loading animation asset

--- assets/lottie/index.ts
+++ assets/lottie/index.ts
@@ -2,4 +2,5 @@ export const Lottie = {
   onboarding1: require('./onboarding_1.json'),
   onboarding2: require('./onboarding_2.json'),
   onboarding3: require('./onboarding_3.json'),
+  loading: require('./loading.json'),
 };
